3a70d4735b02277cf125675ee992c1993b285532
gabrieldss <33532210+gabrieldss@users.noreply.github.com>
2019-09-17 10:18:35 -0300

    Métodos Comuns

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/PrincipalConfigDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/PrincipalConfigDAO.java
@@ -1,4 +1,18 @@
 package br.edu.utfpr.dv.siacoes.dao;
 
 public abstract class PrincipalConfigDAO {
+  import java.sql.ResultSet;
+  import java.sql.SQLException;
+
+public abstract class PrincipalConfigDAO <T> {
+    public T findByDepartment(int idDepartment) throws SQLException {
+        return null;
+    }
+    public int save(int idUser, T config) throws SQLException{
+        return 1;
+    }
+    protected T loadObject(ResultSet rs) throws SQLException{
+        return null;
+    }
+  }
 }
